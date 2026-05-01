2c6f617e3a5437272db93aa5cee33b901ccc8453
Brad Thomson <pwnsmith@live.com>
2026-05-01 13:15:23 +0900

    Shorten combo term

--- keyboards/zsa/voyager/keymaps/senyoku/config.h
+++ keyboards/zsa/voyager/keymaps/senyoku/config.h
@@ -9,6 +9,7 @@
 // --- Combos & One-Shot ---
 #undef ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 3000
+#define COMBO_TERM 20
 
 // --- RGB Matrix Minimalist Settings ---
 #define RGB_MATRIX_STARTUP_SPD 60
